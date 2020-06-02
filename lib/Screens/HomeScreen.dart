9befc3d62f747cdd95b87d2e2f83c313a416e9da
Yashank18 <yashank7696@gmail.com>
2020-06-02 16:22:38 +0530

    Map and other UI

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -20,10 +20,11 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Container(
        child: ListView(
+         physics: BouncingScrollPhysics(),
          children: <Widget>[
            Column(
              children: <Widget>[
-               Container(child: Image.asset("Images/covid.jpg"),width: screenWidth*0.6,),
+               Image.asset("Images/covid.png",width: screenWidth*0.6),
              ],
            ),
            Padding(
@@ -33,6 +34,117 @@ class _HomeScreenState extends State<HomeScreen> {
            Padding(
              padding: const EdgeInsets.all(20.0),
              child: Text("Let's Fight against it together 🤝",style: GoogleFonts.varelaRound(color: Colors.pink,fontWeight: FontWeight.w900,fontSize: screenWidth*0.08,),textAlign: TextAlign.center,),
+           ),
+           SizedBox(height: screenHeight*0.01,),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               Container(
+                 decoration: BoxDecoration(
+                   color: Colors.green.shade400,
+                   borderRadius: BorderRadius.circular(30)
+                 ),
+                 child: Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Text("#GoKarunaGo",style: GoogleFonts.varelaRound(color:Colors.white),),
+               ),
+               ),
+               Container(
+                 decoration: BoxDecoration(
+                   color: Colors.blue.shade400,
+                   borderRadius: BorderRadius.circular(30)
+                 ),
+                 child: Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Text("#LetsFightTogether",style: GoogleFonts.varelaRound(color:Colors.white)),
+               ),
+               ),
+               Container(
+                 decoration: BoxDecoration(
+                   color: Colors.yellow.shade800,
+                   borderRadius: BorderRadius.circular(30)
+                 ),
+                 child: Padding(
+                 padding: const EdgeInsets.all(6.0),
+                 child: Text("#Lockdown",style: GoogleFonts.varelaRound(color:Colors.white)),
+               ),
+               ),
+             ],
+           ),
+           Column(
+             
+             children: <Widget>[
+               SizedBox(height: screenHeight*0.02,),
+               Padding(
+                 padding: const EdgeInsets.all(20.0),
+                 child: Container(
+                   width: screenWidth*0.4,
+                   decoration: BoxDecoration(
+                     color:Color.fromRGBO(217, 26, 77, 1),
+                     borderRadius: BorderRadius.circular(20)
+                   ),
+                   child: Text("India Report",style: GoogleFonts.amaticSc(fontSize: screenHeight*0.04,color: Colors.white,fontWeight: FontWeight.w700),textAlign: TextAlign.center,),
+                 ),
+               ),
+               Image.asset("Images/covidMap.png"),
+
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: Container(
+                   width: screenWidth*0.8,
+                   decoration: BoxDecoration(
+                    color: Colors.yellow.shade800,
+                    borderRadius: BorderRadius.circular(15)
+                   ),
+                   child: Row(
+                     children: <Widget>[
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Text("Confirmed Cases",style: GoogleFonts.varelaRound(fontSize: screenWidth*0.06),),
+                       ),
+                       Padding(
+                         padding: const EdgeInsets.only(left:18.0),
+                         child: Container(
+                           color: Colors.white,
+                           child:Padding(
+                             padding: const EdgeInsets.all(2.0),
+                             child: Text("200000",style: TextStyle(fontSize: screenWidth*0.04),),
+                           )
+                         ),
+                       )
+                     ],
+                   ),
+                 ),
+               ),
+               Padding(
+                 padding: const EdgeInsets.all(8.0),
+                 child: Container(
+                   width: screenWidth*0.8,
+                   decoration: BoxDecoration(
+                    color: Colors.green.shade500,
+                    borderRadius: BorderRadius.circular(15)
+                   ),
+                   child: Row(
+                     children: <Widget>[
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Text("Recovered Cases",style: GoogleFonts.varelaRound(fontSize: screenWidth*0.06,color:Colors.white),),
+                       ),
+                       Padding(
+                         padding: const EdgeInsets.only(left:18.0),
+                         child: Container(
+                           color: Colors.white,
+                           child:Padding(
+                             padding: const EdgeInsets.all(2.0),
+                             child: Text("200000",style: TextStyle(fontSize: screenWidth*0.04),),
+                           )
+                         ),
+                       )
+                     ],
+                   ),
+                 ),
+               ),
+             ],
            )
          ],
        )
